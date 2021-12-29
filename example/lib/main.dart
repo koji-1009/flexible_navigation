d85f2908e8f85bb0b71b36549153f62c28898471
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-29 21:19:37 +0900

    Add dark theme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,9 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const HomePage(),
     );
   }
