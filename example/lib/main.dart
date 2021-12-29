373f58ba04c2d6459b087e62d3a02060d447c512
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-29 18:17:32 +0900

    Update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:breakpoints_mq/breakpoints_mq.dart';
+import 'package:flexible_navigation/flexible_navigation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,103 +15,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    final destinations = [
+      const FlexibleDestination.item(
+        key: Key('first'),
+        label: 'First',
+        icon: Icon(Icons.label),
+        body: Center(
+          child: Text('first'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      const FlexibleDestination.item(
+        key: Key('second'),
+        label: 'Second',
+        icon: Icon(Icons.map),
+        body: Center(
+          child: Text('second'),
+        ),
+      ),
+      const FlexibleDestination.item(
+        key: Key('third'),
+        label: 'Third',
+        icon: Icon(Icons.widgets),
+        body: Center(
+          child: Text('third'),
+        ),
+      ),
+    ];
+
+    return FlexibleScaffold(
+      initialKey: const Key('first'),
+      destinations: destinations,
+      resolver: (size) {
+        final screenSize = Breakpoint.fromWidth(width: size.width).screenSize;
+        switch (screenSize) {
+          case BreakpointScreenSize.extraSmall:
+            return const FlexibleMode.navigationRail(
+              fabInRail: false,
+            );
+          case BreakpointScreenSize.smallFixBody:
+            return const FlexibleMode.drawer(
+              fabInDrawer: false,
+            );
+          case BreakpointScreenSize.smallScaleBody:
+            return const FlexibleMode.navigationBar();
+          case BreakpointScreenSize.medium:
+            return const FlexibleMode.drawer(
+              fabInDrawer: false,
+            );
+          case BreakpointScreenSize.large:
+            return const FlexibleMode.navigationRail(
+              fabInRail: false,
+            );
+        }
+      },
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      floatingActionButton: null,
+      floatingActionButtonLocation: null,
+      floatingActionButtonAnimator: null,
     );
   }
 }
